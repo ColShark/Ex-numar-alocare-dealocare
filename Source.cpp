495d23c99b311807859ed664b68494b554945e8f
ColShark <petre.marius93@gmail.com>
2023-01-30 14:44:12 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void Numar();
+
+int main()
+{
+	Numar();
+
+	system("pause");
+}
+
+void Numar()
+{
+	int numar = 1;
+	std::cout << "Valoarea variabilei numar este " << numar << "." << std::endl;
+}
